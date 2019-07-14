3d82f5b03e70b7548da2e28c3dd64cbc3fc8e6bc
Dawid Wojda <dawiidio@gmail.com>
2019-07-14 22:33:52 +0200

    images support for standard protocols + jsdoc

--- index.js
+++ index.js
@@ -9,23 +9,57 @@ const { createReadStream } = require('fs');
 const PROTOCOL_PREFIX = 'electron-settings';
 const ALLOWED_HOSTS = ['dawiidio.com'];
 
+/**
+ *
+ * @param url {string}
+ * @returns {boolean}
+ */
 const isAllowedHost = url => {
     const { host } = new URL(url);
 
     return ALLOWED_HOSTS.includes(host);
 };
 
+/**
+ *
+ * @param url {string}
+ * @returns {boolean}
+ */
 const isSettingsProtocol = url => {
     const { protocol } = new URL(url);
 
     return protocol === PROTOCOL_PREFIX+':';
 };
 
+/**
+ * Return path with default pathname or path to application module
+ *
+ * @param isFileRequest {boolean}
+ * @param pathname {string}
+ * @param host {string}
+ * @returns {string}
+ */
+const createPathToSource = (isFileRequest, { pathname, host }) =>
+    `${__dirname}/public/${isFileRequest ? pathname.replace('/', '') : host}${isFileRequest ? '' : '.html'}`;
+
+/**
+ *
+ * @param path {string}
+ * @returns {boolean}
+ */
+const isPathToFile = path => path.includes('.');
+
 let window;
 let name;
 
 protocol.registerSchemesAsPrivileged([
-    { scheme: PROTOCOL_PREFIX, privileges: { bypassCSP: true } }
+    {
+        scheme: PROTOCOL_PREFIX,
+        privileges: {
+            bypassCSP: true,
+            standard: true
+        }
+    }
 ]);
 
 app.on('ready', () => {
@@ -47,16 +81,26 @@ app.on('ready', () => {
 
     protocol.registerStreamProtocol(
         PROTOCOL_PREFIX,
-        ({ url, method }, callback) => {
-            const { host } = new URL(url);
-            const pathToPage = `${__dirname}/pages/${host}.html`;
+        (request, callback) => {
+            const headers = {};
+            const { host, pathname } = new URL(request.url);
+            const isFileRequest = isPathToFile(pathname);
+
+            // if you want support more file types please use npm package mime-db or mime-types to check them
+            if (isFileRequest)
+                headers["content-type"] = 'image/jpg';
+            else
+                headers["content-type"] = 'text/html';
+
+            const pathToSource = createPathToSource(isFileRequest, {
+                host,
+                pathname
+            });
 
             callback({
                 statusCode: 200,
-                headers: {
-                    'content-type': 'text/html'
-                },
-                data: createReadStream(pathToPage)
+                headers,
+                data: createReadStream(pathToSource)
             });
         },
         (error) => {
